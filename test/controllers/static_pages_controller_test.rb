0e799f4897ee46392421a9001d227a15cec61343
egpavelka <elwynpavelka@gmail.com>
2016-09-19 05:04:56 -0400

    renamed app, added static pages

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "playsette |"
+  end
+
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "#{@base_title} home"
+  end
+
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+    assert_select "title", "#{@base_title} help"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "#{@base_title} about"
+  end
+
+end
