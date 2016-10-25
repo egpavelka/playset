4c30cf91f89c6f6c64230d83a3b588092bd853a2
egpavelka <elwynpavelka@gmail.com>
2016-10-25 02:50:26 -0400

    add title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Build page titles
+  def full_title(page_title = '')
+    base_title = 'playsette'
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
 end
