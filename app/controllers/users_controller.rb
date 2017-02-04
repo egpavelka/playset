fabbee9ac91bbf98045cc0dcec3daceea7e37e28
egpavelka <pav@egpavelka.com>
2017-02-04 04:56:00 -0500

    added signup form, user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:name, :email, :password, :password_confirmation))
+      if @user.save
+        # success
+      else
+        render 'new'
+      end
   end
 end
