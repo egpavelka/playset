ae14cd503c3801cb71f2e4c0ae0b5f255ef40741
egpavelka <pav@egpavelka.com>
2017-02-06 03:31:03 -0500

    added tracks controller, rerouted root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get 'sessions/new'
 
-  root 'static_pages#home'
+  root 'tracks#top'
   get '/about', to:'static_pages#about'
   get '/help', to:'static_pages#help'
   get '/contact', to:'static_pages#contact'
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  
   resources :users
 end
