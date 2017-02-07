4d785c120ada7ac9830f557ca440739735643260
egpavelka <pav@egpavelka.com>
2017-02-07 02:13:37 -0500

    added assets path for local fonts

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'noreply@playsette.com'
   layout 'mailer'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170206083540) do
+ActiveRecord::Schema.define(version: 20170206123218) do
 
   create_table "submissions", force: :cascade do |t|
     t.integer  "user_id"
@@ -22,11 +22,14 @@ ActiveRecord::Schema.define(version: 20170206083540) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
     t.string   "username"
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.string   "activation_digest"
+    t.boolean  "activated",         default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
