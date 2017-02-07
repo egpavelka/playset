a9212b310fc54daa7085e67fe8f602d8fbb81717
egpavelka <pav@egpavelka.com>
2017-02-07 02:13:37 -0500

    added assets path for local fonts

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
   root 'tracks#top'
+
   get '/about', to:'static_pages#about'
   get '/help', to:'static_pages#help'
   get '/contact', to:'static_pages#contact'
@@ -13,6 +14,9 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  
+
+  get '/tracks/recommended', to: 'tracks#recommended'
+
   resources :users
+  resources :account_activations, only: [:edit]
 end
