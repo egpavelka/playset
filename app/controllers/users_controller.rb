f694c17c704de719ea5f688cb65badbbc7a21d14
egpavelka <pav@egpavelka.com>
2017-02-10 07:58:16 -0500

    show only activated users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_path and return unless @user.activated?
   end
 
   def new
@@ -42,7 +43,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.paginate(page: params[:page], per_page: 10)
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-User.create!(name:  "Test User",
-             username: "test.user",
-             email: "email@testemail.com",
-             password: "TestPassword1",
-             password_confirmation: "TestPassword1",
+User.create!(name:  "Grace Pavelka",
+             username: "egpavelka",
+             email: "egpavelka@gmail.com",
+             password: "Magnolia1",
+             password_confirmation: "Magnolia1",
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
@@ -19,5 +19,7 @@ User.create!(name:  "Test User",
               username: username,
               email: email,
               password: password,
-              password_confirmation: password)
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
 end
