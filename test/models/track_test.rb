adb27d777eb0dfd5960e388e947d97ebe9822400
egpavelka <pav@egpavelka.com>
2017-02-13 03:59:51 -0500

    added Tracks model

---
+++ test/models/track_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class TrackTest < ActiveSupport::TestCase
+
+  def setup
+    @user = users(:testuser)
+    @track = Track.new(url_src: "https://www.youtube.com/watch?v=WMljPLpFUMA", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @track.valid?
+  end
+
+  test "user_id should be present" do
+    @track.user_id = nil
+    assert_not @track.valid?
+  end
+
+end
