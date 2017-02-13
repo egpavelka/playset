1eaa3e7825572291179c054f49021b571904dfa0
egpavelka <pav@egpavelka.com>
2017-02-13 03:20:22 -0500

    remove previous controller and model

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,8 +3,8 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
   test "layout links" do
     get root_path
-    assert_template 'tracks/index'
-    assert_select "a[href=?]", root_path, count: 4
+    assert_template 'static_pages/about'
+    assert_select "a[href=?]", root_path, count: 3
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", help_path
