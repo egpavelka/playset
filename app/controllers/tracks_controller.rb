ebe00e6db3db1257956976f3eba708efa30491b6
egpavelka <pav@egpavelka.com>
2017-02-14 23:43:19 -0500

    added initial track type check

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,2 +1,29 @@
 class TracksController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+  def new
+    @track = Track.new
+  end
+
+  def create
+    @track = current_user.tracks.build(tracks_params)
+    if @track.save
+      flash[:success] = "Track posted successfully."
+      redirect_to root_path
+    else
+      flash[:danger] = "Submission had errors."
+      render 'new'
+    end
+  end
+
+  def index
+    @tracks = Track.paginate(page: params[:page])
+  end
+
+  private
+
+    def tracks_params
+      params.require(:track).permit(:url_src, :title, :artist, :album, :year)
+    end
+
+
 end
