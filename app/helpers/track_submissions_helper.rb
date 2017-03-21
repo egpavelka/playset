d8f905559abf4e78c6bc3dbc2948995504b39964
egpavelka <pav@egpavelka.com>
2017-03-20 23:19:12 -0400

    using inherited classes for step validation in TrackSubmission form object

--- app/helpers/track_submissions_helper.rb
+++ app/helpers/track_submissions_helper.rb
@@ -1,2 +1,40 @@
 module TrackSubmissionsHelper
+
+  # Allowed validations
+  steps = %w(add_source add_media add_metadata)
+
+  def current_step
+    @current_step || steps.first
+  end
+
+  def next_step
+    self.current_step = steps(steps.index(current_step)+1)
+  end
+
+  def previous_step
+    self.current_step = steps(steps.index(current_step)+1)
+  end
+
+  # def set_source
+  #   track.kind = params[:commit]
+  #   # make sure kind corresponds with file/link source field
+  #   # check that it's a valid member of that class
+  #   # if track_loaded? show metadata fields
+  #   # else
+  # end
+  #
+  # def track_loaded?
+  #   # upload_successful? or embed_successful? (got api response?)
+  # end
+  #
+  # def has_metadata?
+  #   # retrieve metadata from embedded sources
+  #   # check mp3 for metadata
+  #   # populate metadata form with retrieved values, disable populated fields
+  # end
+  #
+  # # Track metadata
+  # def track_metadata
+  # end
+
 end
--- app/models/track_submission.rb
+++ app/models/track_submission.rb
@@ -1,67 +1,30 @@
 class TrackSubmission < ApplicationRecord
-  include ActiveModel::Model
 
-  delegate :kind, :status, :title, :artist, :album, :year, to: :track
-  attr_accessor :track, :media_source, :embedded, :upload, :video
+  class SetUp
+    include ActiveModel::Model
 
-  # def initialize
-  #   self.track = Track.new(status: 'new')
-  # end
+    attr_accessor :track
+    delegate :kind, :status, :title, :artist, :album, :year, to: :track
 
-  # check validity of both model and form object
-  def save
-    track_valid = track.valid?
-    if valid? && track_valid
-      track.save
-    else
-      track.errors.each do |field, error|
-        errors.add(field, error)
-      end
-      false
+    def initialize
+      @track = Track.new(track_attributes)
     end
   end
 
+  class Source < SetUp
+    validates :kind, inclusion: { in: %w(Embedded Upload Video) }
+    validates :source_path, presence: true
+  end
 
-  # validates :kind, inclusion: { in: %w(Embedded Upload Video) }, on: :set_source
-  #
-  # def set_source
-  #   track.kind = params[:commit]
-  #   # make sure kind corresponds with file/link source field
-  #   # check that it's a valid member of that class
-  #   # if track_loaded? show metadata fields
-  #   # else
-  # end
-  #
-  # def save_source
-  #   self.save if self.valid?(:set_source)
-  # end
-  #
-  # def save_media
-  #   self.save if self.valid?([:set_source, :set_media])
-  # end
-  #
-  # def save_metadata
-  #   self.save if self.valid?([:set_source, :set_media, :set_metadata])
-  # end
-  #
-  # def track_loaded?
-  #   # upload_successful? or embed_successful? (got api response?)
-  # end
-  #
-  # def has_metadata?
-  #   # retrieve metadata from embedded sources
-  #   # check mp3 for metadata
-  #   # populate metadata form with retrieved values, disable populated fields
-  # end
-  #
-  # # Track metadata
-  # def track_metadata
-  # end
+  class Media < Source
+    validates :media_source, presence: true
+  end
 
-  private
-
-  def persist!
-    @track = current_user.tracks.create!
+  class Metadata < Media
+    validates :title, presence: true
+    validates :album, presence: true
+    validates :artist, presence: true
+    validates :year, length: { is: 4 }
   end
 
 end
