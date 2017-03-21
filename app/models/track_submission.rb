7f61e5cd074581250296ff58a74582b333e835ff
egpavelka <pav@egpavelka.com>
2017-03-21 02:10:44 -0400

    changed views, added routes for submission form actions

--- app/models/track_submission.rb
+++ app/models/track_submission.rb
@@ -1,26 +1,29 @@
 class TrackSubmission < ApplicationRecord
 
+  STEPS = %w(set_up add_source add_media add_metadata).freeze
+
   class SetUp
     include ActiveModel::Model
 
     attr_accessor :track
     delegate :kind, :status, :title, :artist, :album, :year, to: :track
 
-    def initialize
+    def initialize(track_attributes)
       @track = Track.new(track_attributes)
     end
   end
 
-  class Source < SetUp
+
+  class AddSource < SetUp
     validates :kind, inclusion: { in: %w(Embedded Upload Video) }
     validates :source_path, presence: true
   end
 
-  class Media < Source
+  class AddMedia < AddSource
     validates :media_source, presence: true
   end
 
-  class Metadata < Media
+  class AddMetadata < AddMedia
     validates :title, presence: true
     validates :album, presence: true
     validates :artist, presence: true
