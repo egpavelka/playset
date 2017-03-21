60349e61ead8b26056a6cd0176885870022058a9
egpavelka <pav@egpavelka.com>
2017-03-21 02:10:44 -0400

    changed views, added routes for submission form actions

--- app/controllers/track_submissions_controller.rb
+++ app/controllers/track_submissions_controller.rb
@@ -1,14 +1,24 @@
 class TrackSubmissionsController < ApplicationController
-  # before_action :load_
+  # Before 'create' is triggered, run load method.
+  before_action :load_track_submission, except: %i(validate_current)
 
-  def new
-    @submission = TrackSubmission.new
-    @submission.track = Track.new
+  def validate_current
+    @track_submission = current_step_submission(params[:current_step])
+    @track_submission.track.attributes = track_submission_params
+    session[:track_attributes] = @track_submission.track.attributes
+
+    if @track_submission.valid?
+      next_step = next_step_submission(current_step)
+      create and return unless next_step
+      redirect_to action: next_step
+    else
+      flash[:danger] = "There were errors."
+      render current_step
+    end
   end
 
   def create
-    @submission = TrackSubmission.new(params[:track_submission])
-    if @submission.save
+    if @track_submission.save
       # @track.media = params[:kind].safe_constantize.new
       flash[:success] = "Track posted successfully."
       redirect_to root_path
@@ -21,13 +31,22 @@ class TrackSubmissionsController < ApplicationController
   private
 
   def track_submission_params
-    params.require(:track).permit(:status, :kind, :source_path, :title, :artist, :album, :year, media_source_attributes: media_params)
+    params.require(:track_submission).permit(:status, :kind, :source_path, :title, :artist, :album, :year)
   end
 
-  def media_params
-    unless params[:id].nil?
-      Track.find(params[:id]).kind.constantize.column_names
+  def load_track_submission
+    @track_submission = current_step_submission(action_name)
+  end
+
+  def current_step_submission(step)
+    if step.in?(TrackSubmission::STEPS)
+      step(session[:track_attributes])
+    else
     end
+  end
+
+  def next_step_submission(step)
+    track_submission.steps[track_submission.steps.index(step + 1)]
   end
 
 end
