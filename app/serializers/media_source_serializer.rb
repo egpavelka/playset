06b88bb2288290412c5adb8c042bd864d42dfae2
egpavelka <pav@egpavelka.com>
2017-03-25 04:28:39 -0400

    created submission_source column in tracks to solve model nested attribute issue

--- app/serializers/media_source_serializer.rb
+++ app/serializers/media_source_serializer.rb
@@ -1,4 +1,5 @@
 class MediaSourceSerializer < ActiveModel::Serializer
   attributes :media_id, :media_type, :source_path
+  belongs_to :track, serializer: TrackSerializer
   has_one :media, polymorphic: true
 end
