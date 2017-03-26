35c3ce5624ede5277b3b11922899d52a723a1b97
egpavelka <pav@egpavelka.com>
2017-03-26 05:34:29 -0400

    prep/cleanup for external API call service

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,8 +2,13 @@ class Video < ApplicationRecord
   has_one :media_source, as: :media
   has_one :track, through: :media_sources
 
-  # User enters URL
-  # validates :source_path, presence: true
+  attr_accessor :media_source
+
+  VALID_YT_FORMAT = /\A.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/i
+
+  YT_API_KEY = 'AIzaSyD_-CYPWp2DgQ6VeEqPpZtCgQksSWaUU14'
+
+  validates :source_path, presence: true, format: YT_LINK_FORMAT
 
   def source_is_youtube?
     "youtube.com/watch?v=".in? video_source
--- app/serializers/media_source_serializer.rb
+++
@@ -1,5 +0,0 @@
-class MediaSourceSerializer < ActiveModel::Serializer
-  attributes :media_id, :media_type, :source_path
-  belongs_to :track, serializer: TrackSerializer
-  has_one :media, polymorphic: true
-end
