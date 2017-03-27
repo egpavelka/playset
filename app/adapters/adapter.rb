dd8f8e6a4229ae754d5e13c6fe25ae63d41c1515
egpavelka <pav@egpavelka.com>
2017-03-27 12:48:18 -0400

    more structuring of the various API calls under multiple MediaSource types

--- app/adapters/adapter.rb
+++ app/adapters/adapter.rb
@@ -5,10 +5,19 @@ module Adapters
 ## VIDEO
   class API < Adapter
 
-    attr_accessor :video
+    attr_accessor :video, :embedded
 
     def parse_source(source_path)
       # Find parameters for API lookup within supplied source path
+      #
+      # all api calls need:
+      # valid_format regex (verify input)
+      # list of required uri parameters (with method captures to pull from input)
+      # uri structure, build formula
+      # call
+      # response verification
+      # json structure and attributes to pull for metadata and player generation
+      # show: build embedded video/song player
     end
 
     def build_uri
--- app/models/video.rb
+++ app/models/video.rb
@@ -7,7 +7,6 @@ class Video < ApplicationRecord
   VALID_YT_FORMAT = /\A.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/i
   # https://www.youtube.com/watch?v=8KkW8Ul7Q1I
   # https://youtu.be/8KkW8Ul7Q1I?t=1m
-  # self.yt_id = source.match.(VALID_YT_FORMAT).captures[1]
   # First match group will be 'watch?v=' or '&v='
   # Second match group will be video ID
 
@@ -15,5 +14,30 @@ class Video < ApplicationRecord
 
   # validates :source_path, presence: true, format: VALID_YT_FORMAT
 
+  def assign_api
+    if VALID_YT_FORMAT.match?(source_path)
+      api = 'youtube'
+      video_id(source_path)
+    # elsif VALID_VIMEO_FORMAT.math?(source_path)
+    #   api = 'vimeo'
+    #   ...extracted query params...
+    else
+      errors.add(:video, "Please provide a valid YouTube or Vimeo video link.")
+
+  end
+
+
+end
+
+class YouTube < Video
+
+  def video_id
+    source_path.match.(VALID_YT_FORMAT).captures[1]
+  end
+
+  def query_params
+    # way to take arbitray number of arguments in controller/Adapter
+    # api.to_constantize.new()
+  end
 
 end
