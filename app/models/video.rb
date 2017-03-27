17df197ab3b13c9f8cac771af0c256aa7eed0e93
egpavelka <pav@egpavelka.com>
2017-03-27 06:07:08 -0400

    validation for youtube link formats

--- app/models/video.rb
+++ app/models/video.rb
@@ -5,13 +5,15 @@ class Video < ApplicationRecord
   attr_accessor :media_source
 
   VALID_YT_FORMAT = /\A.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/i
+  # https://www.youtube.com/watch?v=8KkW8Ul7Q1I
+  # https://youtu.be/8KkW8Ul7Q1I?t=1m
+  # self.yt_id = source.match.(VALID_YT_FORMAT).captures[1]
+  # First match group will be 'watch?v=' or '&v='
+  # Second match group will be video ID
 
   YT_API_KEY = 'AIzaSyD_-CYPWp2DgQ6VeEqPpZtCgQksSWaUU14'
 
-  validates :source_path, presence: true, format: YT_LINK_FORMAT
+  # validates :source_path, presence: true, format: VALID_YT_FORMAT
 
-  def source_is_youtube?
-    "youtube.com/watch?v=".in? video_source
-  end
 
 end
