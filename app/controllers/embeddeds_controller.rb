323b7eb1c57dbdc5aaddac4f7ffe08c8361b2a23
egpavelka <pav@egpavelka.com>
2017-03-31 04:40:27 -0400

    added URL formats for Bandcamp and Spotify tracks

--- app/controllers/embeddeds_controller.rb
+++ app/controllers/embeddeds_controller.rb
@@ -7,12 +7,17 @@ class EmbeddedsController < ApplicationController
 end
 
 class BandcampController < EmbeddedsController
+  VALID_BANDCAMP_FORMAT = /^[https?:\/\/]+([a-z]+)\.bandcamp\.com\/track\/([^#\&\?\/]*)/i
+  # https://pwelverumandsun.bandcamp.com/track/real-death
+  # (first match group is band name, second is track)
 end
 
 class SoundcloudController < EmbeddedsController
 end
 
 class SpotifyController < EmbeddedsController
+  VALID_SPOTIFY_FORMAT = /^(spotify:track:|https?:\/\/[a-z]+\.spotify\.com\/track\/)([^#\&\?\/]*)/i
+
   # spotify:track:2TpxZ7JUBn3uw46aR7qd6V
-  # http://open.spotify.com/track/2TpxZ7JUBn3uw46aR7qd6V
+  # http://open.spotify.com/track/2TpxZ7JUBn3uw46aR7qd6V (or https)
 end
