87091443be224789b45749e6134a8f44016d238e
egpavelka <pav@egpavelka.com>
2017-04-04 15:02:14 -0400

    set up internal methods for media controllers to store strings from url input for api query

--- app/models/embedded.rb
+++ app/models/embedded.rb
@@ -4,64 +4,58 @@ class Embedded < ApplicationRecord
 
   VALID_BANDCAMP_FORMAT = /^[https?:\/\/]+([a-z]+)\.bandcamp\.com\/track\/([^#\&\?\/]*)/i
   # https://ARTIST.bandcamp.com/track/TITLE
-  # (first match group is artist, second is title)
 
   VALID_SOUNDCLOUD_FORMAT = /^[https?:\/\/\soundcloud\.com]+\/([^#\&\?\/]*)\/([^#\&\?\/]*)/
   # https://soundcloud.com/ARTIST/TITLE
-  # (first match group is artist, second is title)
 
   VALID_SPOTIFY_FORMAT = /^(spotify:track:|https?:\/\/[a-z]+\.spotify\.com\/track\/)([^#\&\?\/]*)/i
   # spotify:track:2TpxZ7JUBn3uw46aR7qd6V
   # http://open.spotify.com/track/2TpxZ7JUBn3uw46aR7qd6V (or https)
-  # (first match group is type (uri vs url), second is track id)
 
   # Assign to service so API can be called
   def assign_api
-
+    embedded_sources = {
+      VALID_BANDCAMP_FORMAT => :bandcamp,
+      VALID_SOUNDCLOUD_FORMAT => :soundcloud,
+      VALID_SPOTIFY_FORMAT => :spotify
+    }
   end
-
-  def new
-    @embedded = Embedded.new()
-  end
-
 
 end
 
 class Bandcamp < Embedded
 
-  def create
-    @embedded = Embedded.new(bandcamp_params)
+  def artist_path
+    source_path.match.(VALID_BANDCAMP_FORMAT).captures[0]
+    # regex conversion to lookup syntax
   end
 
-  private
-  def bandcamp_params
-    params.require(:bandcamp).permit(:artist, :title)
+  def title_path
+    source_path.match.(VALID_BANDCAMP_FORMAT).captures[1]
+    # regex conversion to lookup syntax
   end
 
 end
 
 class Soundcloud < Embedded
 
-  def create
-    @embedded = Embedded.new(soundcloud_params)
+  def artist_path
+    source_path.match.(VALID_SOUNDCLOUD_FORMAT).captures[0]
+    # regex conversion to lookup syntax
   end
 
-  private
-  def soundcloud_params
-    params.require(:soundcloud_params).permit(:artist, :title)
+  def title_path
+    source_path.match.(VALID_SOUNDCLOUD_FORMAT).captures[1]
+    # regex conversion to lookup syntax
   end
 
 end
 
 class Spotify < Embedded
 
-    def create
-      @embedded = Embedded.new(spotify_params)
-    end
-
-    private
-    def spotify_params
-      params.require(:spotify).permit(:track_id)
-    end
+  def track_id
+    source_path.match.(VALID_YT_FORMAT).captures[1]
+    # (first match group is type (uri vs url), second is track id)
+  end
 
 end
