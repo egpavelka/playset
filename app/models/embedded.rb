73c8fc536bfaecccde364a9a24c7c4e81aaaa8fa
egpavelka <pav@egpavelka.com>
2017-04-04 05:44:18 -0400

    add params to embedded controllers

--- app/models/embedded.rb
+++ app/models/embedded.rb
@@ -29,10 +29,39 @@ end
 
 class Bandcamp < Embedded
 
+  def create
+    @embedded = Embedded.new(bandcamp_params)
+  end
+
+  private
+  def bandcamp_params
+    params.require(:bandcamp).permit(:artist, :title)
+  end
+
 end
 
 class Soundcloud < Embedded
+
+  def create
+    @embedded = Embedded.new(soundcloud_params)
+  end
+
+  private
+  def soundcloud_params
+    params.require(:soundcloud_params).permit(:artist, :title)
+  end
+
 end
 
 class Spotify < Embedded
+
+    def create
+      @embedded = Embedded.new(spotify_params)
+    end
+
+    private
+    def spotify_params
+      params.require(:spotify).permit(:track_id)
+    end
+
 end
