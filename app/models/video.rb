1c298f43afcd831d2e9e8fc62f13f1bf5b85eb2d
egpavelka <pav@egpavelka.com>
2017-04-14 23:54:32 -0400

    api base urls moved to respective models

--- app/models/video.rb
+++ app/models/video.rb
@@ -15,18 +15,28 @@ end
 
 class YouTube < Video
 
+  # API parameters from input url
   def video_id
     source_path.match.(VALID_YT_FORMAT).captures[1]
     # First match group will be 'watch?v=' or '&v='
     # Second match group will be video ID
   end
 
+  # API url structure with parameters
+  def api_url
+  end
+
 end
 
 class Vimeo < Video
 
+  # API parameters from input url
   def video_id
     source_path.match(VALID_SPOTIFY_FORMAT).captures[0]
+  end
+
+  # API url structure with parameters
+  def api_url
   end
 
 end
