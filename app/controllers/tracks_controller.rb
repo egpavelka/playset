f06376fe889eed38c53cf69ff6d7cd3a7a332437
egpavelka <pav@egpavelka.com>
2017-05-17 10:06:09 -0400

    updated track submission form

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -10,16 +10,16 @@ class TracksController < ApplicationController
   def create
     @track = current_user.tracks.create!(track_params)
     if @track.save
-      flash[:success] = "Track posted successfully."
-      redirect_to root_path
-    # Check for errors in submission source first
-    elsif track.errors[:set_media_source]
-      flash[:error] = "There was an error with the link submitted."
-      render 'new'
-    # Then check for presence of metadata in validated media
-    elsif track.errors[:source_has_metadata]
-      flash[:error] = "Metadata could not be retrieved.  Please fill out the fields below."
-      render 'add_metadata'
+      # If metadata was retrieved from source, publish the track and render its view.
+      if @track.source_has_metadata?
+        @track.state = 'published'
+        flash[:success] = "Track posted successfully."
+        redirect_to root_path
+      # If media was created successfully from source but metadata was not retrieved, track state remains as 'draft' and edit page is rendered.
+      else
+        flash[:error] = "Metadata could not be retrieved.  Please fill out the fields below."
+        render 'edit'
+      end
     # Catch any other errors
     else
       flash[:error] = "Submission had errors."
@@ -27,12 +27,17 @@ class TracksController < ApplicationController
     end
   end
 
-  def show
-    @track = Track.find(params[:id]).media_sources.media
+  def edit
+    @track = Track.find(params[:id])
   end
 
-  def index
-    @tracks = Track.paginate(page: params[:page])
+  def update
+    if @track.update_attributes(track_params)
+      flash[:success] = "Your profile has been updated."
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def destroy
@@ -43,10 +48,21 @@ class TracksController < ApplicationController
     redirect_to submitter_path
   end
 
+  def show
+    @track = Track.find(params[:id]).media_sources.media
+  end
+
+  def index
+    @tracks = Track.paginate(page: params[:page])
+  end
+
   private
 
   def track_params
     params.require(:track).permit(:status, :kind, :submission_source, :title, :artist, :album, :year, :album_art)
+  end
+
+  def source_has_metadata?
   end
 
 end
