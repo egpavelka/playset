205f326617a578e78d989e4b59ba0c3dde3fee93
egpavelka <pav@egpavelka.com>
2017-05-19 02:07:03 -0400

    validation of embedded links

--- config/routes.rb
+++ config/routes.rb
@@ -23,10 +23,12 @@ Rails.application.routes.draw do
   post '/submit', to: 'tracks#create'
 
 
+
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :tracks
+
 
   # resources :tracks do
   #   resources :embeddeds, :uploads, :videos
