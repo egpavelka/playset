a0656f61468b55601ffcf255082e6f032438e315
egpavelka <pav@egpavelka.com>
2017-06-03 08:40:07 -0400

    submissions complete for included metadata

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,36 +1,10 @@
 class TracksController < ApplicationController
-  # before_action :set_media
 
   def new
-    # @track = @media.track.create
-    @track = Track.new(track_params)
-    # @track.media =
-  end
-
-  def submit
+    @track = Track.new
   end
 
   def create
-  # Initialize media params
-  # @track.media_sources.build(media: media_kind, source_path: submission_source)
-    @track = current_user.tracks.create!(track_params)
-    if @track.save
-      # If metadata was retrieved from source, publish the track and render its view.
-      if source_has_metadata?
-        @track.state = 'published'
-        flash[:success] = "Track posted successfully."
-        redirect_to root_path
-      # If media was created successfully from source but metadata was not retrieved, track state remains as 'draft' and edit page is rendered.
-      else
-        flash[:error] = "Metadata could not be retrieved.  Please fill out the fields below."
-        @track.new_record?
-        render 'edit'
-      end
-    # Catch any other errors
-    else
-      flash[:error] = "Submission had errors."
-      render 'new'
-    end
   end
 
   def edit
