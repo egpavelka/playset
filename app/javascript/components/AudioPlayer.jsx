dd587eeb56d1a5d8bc9795c0d663a0b7b15fb475
egpavelka <pav@egpavelka.com>
2017-07-13 18:38:46 -0400

    add playback handling exception for Soundcloud tracks

--- app/javascript/components/AudioPlayer.jsx
+++ app/javascript/components/AudioPlayer.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PlayButton from './track/play_button'
+import { soundcloudPublicClientId } from '../assets/secrets'
 
 class AudioPlayer extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      trackAudio: new Audio(this.props.track.mediaPath),
       isPlaying: false,
       nextAction: 'play'
     }
+    this.setAudio = this.setAudio.bind(this);
+    this.setSoundcloudUrl = this.setSoundcloudUrl.bind(this);
     this.togglePlayState = this.togglePlayState.bind(this);
+
   }
 /* THINGS THAT CLEAR this.state.currentTrack
 finished // track has played in full
@@ -19,22 +22,40 @@ play on different Track
 recently listened: if track play time > 30sec
 
 */
+  setAudio() {
+    if (this.props.embeddedService == 'Soundcloud') {
+      this.setSoundcloudUrl()
+    } else if (this.state.trackAudio == undefined) {
+      this.setState({ trackAudio: new Audio(this.props.track.media_path) })
+    }
+  }
 
+  setSoundcloudUrl() {
+    var callUrl = this.props.track.media_path + '?client_id=' +  soundcloudPublicClientId
+    fetch(callUrl)
+    .then((response) => { this.setState({ trackAudio: new Audio(response.url) }) })
+    .catch((error) => { console.log(error) })
+  }
+
+  componentWillMount() {
+
+  }
   componentDidMount() {
-    this.state.trackAudio.addEventListener('ended', function(e){ console.log(e) })
-    this.state.trackAudio.addEventListener('timeupdate', function(e){ console.log(e) })
+    this.setAudio()
+    console.log(this.state.trackAudio)
+    // this.state.trackAudio.addEventListener('ended', function(e){ console.log(e) })
+    // this.state.trackAudio.addEventListener('timeupdate', function(e){ console.log(e) })
   }
 
   togglePlayState() {
-    var trackAudio = this.state.trackAudio
     console.log(this.state.isPlaying)
     if (this.state.isPlaying) {
-      trackAudio.pause()
-      this.setState({ isPlaying: false })
+      this.state.trackAudio.pause()
+      this.setState({ isPlaying: false, nextAction: 'play' })
     } else {
-      console.log("i know")
-      trackAudio.play()
-      this.setState({ isPlaying: true })
+      this.setAudio()
+      this.state.trackAudio.play()
+      this.setState({ isPlaying: true, nextAction: 'pause' })
     }
   }
 
@@ -47,5 +68,9 @@ recently listened: if track play time > 30sec
     )
   }
 }
+
+// AudioPlayer.propTypes = {
+//
+// }
 
 export default AudioPlayer
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Playsette
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # Automatically camelize props when passed to React
+    config.react.camelize_props = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
