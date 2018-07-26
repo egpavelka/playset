fec0169bc7807cf9e040161f3ee380ef32733c7d
egpavelka <pav@egpavelka.com>
2018-07-26 07:27:56 -0400

    added track submission forms

--- client/src/containers/tracks/CreateTrack.js
+++ client/src/containers/tracks/CreateTrack.js
@@ -1,47 +1,80 @@
 import React from 'react'
 import { Button, Form } from 'reactstrap'
 import FormInput from '../application/Forms/FormInput'
+import gql from 'graphql-tag'
+import { Mutation } from 'react-apollo'
 
+const CHECK_TRACK_MUTATION = gql`
+  mutation CheckTrackMutation($submittedUrl: String!) {
+    check_track(submittedUrl: $submittedUrl) {
+      // ...
+    }
+  }
+`
+const CREATE_TRACK_MUTATION = gql`
+mutation CreateTrackMutation($submittedUrl: String!, $mediaUrl: !String, $title: String! $artist: String!, $album: String!, $year: String!, $sourceService: String!, $sourceServiceId: String!, $mediaType: String!, userId: String!) {
+  create_track(submittedUrl: $submittedUrl, mediaUrl: $mediaUrl, title: $title, artist: $artist, album: $album, year: $year, sourceService: $sourceService, sourceServiceId: $sourceServiceId, mediaType: $mediaType, userId: $userId) {
+    // ...
+  }
+}
+`
 class CreateTrack extends React.Component {
   state = {
+    valid: '',
     submittedUrl: '',
     mediaUrl: '',
-    title: '',
-    artist: '',
-    album: '',
-    year: '',
     sourceService: '',
     sourceServiceId: '',
     mediaType: '',
-    userId: ''
+    userId: '',
+    title: '',
+    artist: '',
+    album: '',
+    year: ''
   }
 
   render () {
-    const { submittedUrl, mediaUrl, title, artist, album, year, sourceService, sourceServiceId, mediaType, userId } = this.state
+    const { submittedUrl, title, artist, album, year } = this.state
     return (
-        <Form>
-        <FormInput fieldName='submittedUrl'
-      label='url'
-      value={ this.state.submittedUrl }
-      update={ e => this.setState(
-        { submittedUrl: e.target.value })}/>
-        <FormInput fieldName='title'
-      value={ this.state.title }
-      update={ e => this.setState(
-        { title: e.target.value })}/>
-        <FormInput fieldName='artist'
-      value={ this.state.artist }
-      update={ e => this.setState(
-        { artist: e.target.value })}/>
-        <FormInput fieldName='album'
-      value={ this.state.album }
-      update={ e => this.setState(
-        { album: e.target.value })}/>
-        <FormInput fieldName='year'
-      value={ this.state.year }
-      update={ e => this.setState(
-        { year: e.target.value })}/>
-        </Form>
+      <Form>
+        {!valid && (
+           <FormInput fieldName='submittedUrl'
+                      label='url'
+                      value={ submittedUrl }
+                      update={ e => this.setState(
+                          { submittedUrl: e.target.value })}/>
+        )}
+        {valid && (
+           <FormInput fieldName='title'
+                      value={ title }
+                      update={ e => this.setState(
+                          { title: e.target.value })}/>
+           <FormInput fieldName='artist'
+                      value={ artist }
+                      update={ e => this.setState(
+                          { artist: e.target.value })}/>
+           <FormInput fieldName='album'
+                      value={ album }
+                      update={ e => this.setState(
+                          { album: e.target.value })}/>
+           <FormInput fieldName='year'
+                      value={ year }
+                      update={ e => this.setState(
+                          { year: e.target.value })}/>
+        )}
+      </Form>
     )
   }
+
+  _confirm = async data => {
+    /* const { token } = this.state.login ? data.login : data.signup
+     * this._saveUserData(token)
+     * this.props.history.push(`/`)
+     */ }
+
+  _saveUserData = token => {
+    /* localStorage.setItem(AUTH_TOKEN, token)*/
+  }
 }
+
+export default CreateTrack
