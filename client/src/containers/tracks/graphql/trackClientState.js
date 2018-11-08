ce5cf9531f2f87aae2075d421090fea5cd76caed
egpavelka <pav@egpavelka.com>
2018-11-08 06:16:54 -0500

    update current track cache writes

--- client/src/containers/tracks/graphql/trackClientState.js
+++ client/src/containers/tracks/graphql/trackClientState.js
@@ -14,3 +14,14 @@ export const GET_CURRENT_TRACK = gql`
     currentTrack @client
   }
 `
+export const SET_CURRENT_TRACK = gql``
+
+export const CLEAR_CURRENT_TRACK = gql``
+
+export const setCurrentTrack = (_obj, { item }, { cache }) => {
+  cache.writeQuery({ query, data: { currentTrack: item} })
+}
+
+export const clearCurrentTrack = (_obj, { item }, { cache }) => {
+  cache.writeQuery({ query, data: { currentTrack: null } })
+}
