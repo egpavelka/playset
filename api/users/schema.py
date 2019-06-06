4a9eab26007cba214b372a35477934ee5dd84782
gigi <e.g. pavelka>
2019-06-06 10:54:54 -0400

    syntax fix

--- api/users/schema.py
+++ api/users/schema.py
@@ -28,7 +28,7 @@ class CreateUser(graphene.Mutation):
     user = graphene.Field(UserType)
 
     class Arguments:
-t        username = graphene.String(required=True)
+        username = graphene.String(required=True)
         email = graphene.String(required=True)
         password = graphene.String(required=True)
         firstName = graphene.String()
